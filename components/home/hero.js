1f3adb4f351fc866d6c6180243fef2bc9ae4e3bc
kjw <kjw@example.com>
2022-12-08 00:35:24 +0900

    애니메이션이 안되요.

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,12 +1,13 @@
 import Animation from "./animation";
 import Link from "next/link";
+import Image from 'next/image'
 import MovingComponent from "react-moving-text";
 
 export default function Hero() {
     return (
         <>
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            <div className="flex-grow   flex flex-col  md:text-left  md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl font-medium text-gray-900 flex items-center text-center">
                 <MovingComponent 
                     type="typewriter"
                     dataText={[
@@ -15,7 +16,8 @@ export default function Hero() {
                     ]} 
                 />
             </h1>
-            <p className="ml-3 mb-8 leading-relaxed">저는 풀스택 웹 개발자를 꿈꾸고 있습니다.
+            <br/>
+            <p className="ml-3 mb-8 leading-relaxed flex items-center text-center">저는 풀스택 웹 개발자를 꿈꾸고 있습니다.
                 <br/>항상 새로운 것을 배울 때 재미를 느낍니다.
                 <br/>언제나 새로운 것을 좋아하며, 관심이 가고 즐기게됩니다.
                 <br/>오늘에 만족하지 않고 더 나은 내일을 위해 
@@ -28,9 +30,6 @@ export default function Hero() {
                     <a className="btn-project">&lt;Projects /&gt;</a>
                 </Link>
             </div>
-            </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <Animation/>
             </div>
         </>
     );
